7bef85fd0c8a40945ab70d26b16f7eb14fcc4b5f
officialim <141108672+officialim@users.noreply.github.com>
2025-01-11 21:42:14 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -10,7 +10,7 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         
-    def new_user(self,name):
+    def new_user(self,id,first_name):
         return dict(
             id=id,
             name=name,
