9b874fd5ebec58b4ad7ee8792b72fee0207bd473
officialim <141108672+officialim@users.noreply.github.com>
2025-01-11 19:11:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 # Bot information
 API_ID = environ.get('API_ID', "25603034")
 API_HASH = environ.get('API_HASH', "294a7bf4488b21609436de1cdd05c316")
-BOT_TOKEN = environ.get('BOT_TOKEN', "6323175051:AAETlfTkPGUPfViVkFUMSLJ3jld2iJNLp1g")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7801268874:AAHP4sresLeFirkh7WgcZ6Ko5ufHhL9Gl4Y")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5344594309').split()]
@@ -30,7 +30,7 @@ INDEX_USER.extend(ADMINS)
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Abcd:abcd1234@cluster0.zsmqgc6.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "pimon")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'files')
 #Database.add_user(DB_NAME)
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001572413887'))
