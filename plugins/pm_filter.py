7fe687d17455caf5dbdbf46e7a2be20b3952ea19
officialim <141108672+officialim@users.noreply.github.com>
2025-01-11 22:53:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -29,10 +29,10 @@ SPELL_CHECK = {}
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def filters_private_handlers(client, message):
 
-    if not await db.is_user_exist(message.from_user.name):
+    if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
 
-    if message.text.startswith(("/", "@")):
+    if message.text.startswith("/"):
         return
     
     url_pattern = re.compile(r'https?://\S+')
@@ -97,7 +97,14 @@ async def filters_private_handlers(client, message):
         await db.reset_verification_status()
     
     if maintenance_mode is True:
-        await message.reply_text(f"<b>Sorry for the inconvenience, we are under Maintenance. We'll be back soon!</b>", disable_web_page_preview=True)
+        btn = [
+                [InlineKeyboardButton("Search", url=f"https://t.me/flimrobot")],
+                [InlineKeyboardButton("Request", url=f"https://t.me/PrimehubReq")],
+            ]
+        await message.reply_text(
+            f"<b>This bot has been unmaintained for long time. Please refer to @FlimRobot for the latest movies and series.\n\nএই বটটি দীর্ঘদিন ধরে অপরিচলিত রয়েছে। নতুন সিনেমা অ্যান্ড সিরিজ পেতে এই বটটি ভিসিট করুন @FlimRobot\n\nये बॉट काफी समय से बंद है. नई फ़िल्में और सीरीज़ @FlimRobot पाने के लिए इस बॉट पर जाएँ</b>",
+            reply_markup=InlineKeyboardMarkup(btn),
+            disable_web_page_preview=True)
         return
     
     if private_filter is False:
@@ -459,9 +466,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup=reply_markup,
         disable_web_page_preview=True,
         )
-        if not await db.is_user_exist(query.from_user.name):
+        if not await db.is_user_exist(query.from_user.id):
             await db.add_user(
-                query.from_user.id.name,
+                query.from_user.id,
                 query.from_user.first_name
                 )
         
@@ -636,7 +643,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         truncated_messages = []
 
         for msg in top_searches:
-            if msg.lower() not in unique_messages and is_valid_string(msg) and not msg.startswith('@'):
+            if msg.lower() not in unique_messages and is_valid_string(msg):
                 unique_messages.add(msg.lower())
 
                 files, _, _ = await get_search_results(msg.lower())
