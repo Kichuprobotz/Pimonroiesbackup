6603645b0a4da137b807279f7ecdc37fed67ac2e
officialim <141108672+officialim@users.noreply.github.com>
2025-01-11 19:32:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -459,7 +459,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup=reply_markup,
         disable_web_page_preview=True,
         )
-        if not await db.is_user_exist(query.from_user.id):
+        if not await db.is_user_exist(query.from_user.name.id):
             await db.add_user(
                 query.from_user.id,
                 query.from_user.first_name
