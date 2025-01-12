d3378ee30dba5d1a4f2a2fcc864948aa2d1b4c83
officialim <141108672+officialim@users.noreply.github.com>
2025-01-12 17:43:48 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -13,7 +13,7 @@ class Database:
     def new_user(self, id, name):
         return dict(
             id = id,
-            name = name,
+            name = first_name,username,
             Premium=False, 
             premium_expiry=None, 
             purchase_date=None, 
