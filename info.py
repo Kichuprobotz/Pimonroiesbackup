7919fd2add03083a7a82f353f94061de569e4735
officialim <141108672+officialim@users.noreply.github.com>
2025-05-31 23:50:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 # Bot information
 API_ID = environ.get('API_ID', "25603034")
 API_HASH = environ.get('API_HASH', "294a7bf4488b21609436de1cdd05c316")
-BOT_TOKEN = environ.get('BOT_TOKEN', "8052979840:AAGWRHeox6MeobvT13ZVJVKkF_DSFxYxO6U")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8052979840:8152850410:AAG1sgZhag4QXCkGFvhIMXeZ387MfD-qIII")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1669823187').split()]
@@ -33,7 +33,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "filmzone")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'filesgram')
 #Database.add_user(DB_NAME)
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001572413887'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002469255122'))
 FORCESUB_CHANNEL = int(environ.get('FORCESUB_CHANNEL', "0"))
 SLOW_MODE_DELAY = int(environ.get('SLOW_MODE_DELAY', 10))
 WAIT_TIME = int(environ.get('AUTO_DELETE_WAIT_TIME', 600))
