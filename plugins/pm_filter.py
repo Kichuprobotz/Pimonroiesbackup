581126eae165640c293d900052370f9686491fe5
officialim <141108672+officialim@users.noreply.github.com>
2025-06-23 14:09:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -30,7 +30,9 @@ SPELL_CHECK = {}
 async def filters_private_handlers(client, message):
 
     if not await db.is_user_exist(message.from_user.id):
-            await db.add_user(message.from_user.id, message.from_user.name, message.from_user.first_name)
+       user_name = message.from_user.name if message.from_user.name else message.from_user.first_name or "Unknown"
+             await db.add_user(message.from_user.id, user_name, message.from_user.first_name)
+
 
     if message.text.startswith(("/", "@")):
         return
